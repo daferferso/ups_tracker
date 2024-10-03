c980c65ab01c7574ab8586b487f1d015d1153193
daferferso <daferferso@gmail.com>
2024-10-02 21:54:14 -0400

    ✨ Add CORS support and vercel.json for deployment configuration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger.js";
@@ -7,6 +8,7 @@ import upsRouter from "./routes/upsRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
